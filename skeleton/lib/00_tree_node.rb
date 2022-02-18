fae6cc2e816a5a5a926d16a47beb630bc1907b5a
App Academy Student <email@email.com>
2022-02-18 13:38:30 -0500

    modified parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,43 @@
-class PolyTreeNode
+require 'byebug'
 
+class PolyTreeNode
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    return @parent = nil if new_parent.nil?
+    old_parent = parent
+    
+    if !parent.nil?
+      @parent = new_parent
+      old_parent.children.delete(self)
+      
+    else
+      # parent.children.shift
+      @parent = new_parent
+      
+    end
+    parent.children.push(self)
+  end
 end
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
